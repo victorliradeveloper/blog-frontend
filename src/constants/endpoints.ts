089c337b2547cdde4934b7fe264b74442510551a
victorlirafront <victorliradeveloper@gmail.com>
2025-07-13 17:46:55 -0300

    Revert "chore: update backend endpoint"
    This reverts commit 0d42e6d3544014755b7a6d3aa72254124ff4a745.

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,3 +1,3 @@
 // export const DEV_API_URL = 'http://host.docker.internal:20787';
-export const DEV_API_URL = 'https://blog-backend-production-88d3.up.railway.app/';
-export const PROD_API_URL = 'https://blog-backend-production-88d3.up.railway.app/';
+export const DEV_API_URL = 'https://blog-simple-api-production.up.railway.app/';
+export const PROD_API_URL = 'https://blog-simple-api-production.up.railway.app/';
