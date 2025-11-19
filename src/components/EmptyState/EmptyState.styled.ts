cf4013ea4ae9ca0c445567635f781292a53f83e2
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-11-19 18:43:59 -0300

    refactor: improve EmptyState component structure using styled-components

--- src/components/EmptyState/EmptyState.styled.ts
+++ src/components/EmptyState/EmptyState.styled.ts
@@ -1,7 +1,7 @@
 'use client';
 import styled from 'styled-components';
 
-export const StyledEmptyState = styled.div`
+export const StyledEmptyState = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -14,69 +14,46 @@ export const StyledEmptyState = styled.div`
   @media screen and (max-width: 768px) {
     padding: 150px 20px 80px;
   }
+`;
 
-  .icon-wrapper {
-    width: 120px;
-    height: 120px;
-    margin-bottom: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: rgba(7, 235, 176, 0.1);
+export const IconWrapper = styled.div`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(7, 235, 176, 0.1);
+  border-radius: 50%;
+  position: relative;
+
+  @media screen and (max-width: 768px) {
+    width: 100px;
+    height: 100px;
+    margin-bottom: 25px;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: 2px solid rgba(7, 235, 176, 0.3);
     border-radius: 50%;
+    animation: pulse 2s infinite;
+  }
+
+  img {
+    width: 60px;
+    height: 60px;
     position: relative;
+    z-index: 1;
+    filter: brightness(0) saturate(100%) invert(64%) sepia(89%) saturate(1352%) hue-rotate(120deg)
+      brightness(101%) contrast(101%);
 
     @media screen and (max-width: 768px) {
-      width: 100px;
-      height: 100px;
-      margin-bottom: 25px;
-    }
-
-    &::before {
-      content: '';
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      border: 2px solid rgba(7, 235, 176, 0.3);
-      border-radius: 50%;
-      animation: pulse 2s infinite;
-    }
-
-    img {
-      width: 60px;
-      height: 60px;
-      position: relative;
-      z-index: 1;
-      filter: brightness(0) saturate(100%) invert(64%) sepia(89%) saturate(1352%) hue-rotate(120deg)
-        brightness(101%) contrast(101%);
-
-      @media screen and (max-width: 768px) {
-        width: 50px;
-        height: 50px;
-      }
-    }
-  }
-
-  h1 {
-    font-size: 32px;
-    font-weight: 600;
-    color: #fff;
-    margin-bottom: 16px;
-    line-height: 1.3;
-
-    @media screen and (max-width: 768px) {
-      font-size: 24px;
-    }
-  }
-
-  p {
-    font-size: 16px;
-    color: #8f9ba8;
-    line-height: 1.6;
-    margin: 0;
-
-    @media screen and (max-width: 768px) {
-      font-size: 14px;
+      width: 50px;
+      height: 50px;
     }
   }
 
@@ -93,6 +70,31 @@ export const StyledEmptyState = styled.div`
       transform: scale(1);
       opacity: 1;
     }
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  color: #fff;
+  margin: 0 0 16px 0;
+  padding: 0;
+  line-height: 1.3;
+
+  @media screen and (max-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const Message = styled.p`
+  font-size: 16px;
+  color: #8f9ba8;
+  line-height: 1.6;
+  margin: 0;
+  padding: 0;
+
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
   }
 `;
 
--- src/components/EmptyState/index.tsx
+++ src/components/EmptyState/index.tsx
@@ -1,15 +1,15 @@
-import { StyledEmptyState } from './EmptyState.styled';
+import { StyledEmptyState, IconWrapper, Title, Message } from './EmptyState.styled';
 import { EmptyStateProps } from './types';
 import Image from 'next/image';
 
 const EmptyState = function ({ title, message, icon = '/error.png' }: EmptyStateProps) {
   return (
     <StyledEmptyState>
-      <div className="icon-wrapper">
+      <IconWrapper>
         <Image src={icon} alt="Empty state icon" width={60} height={60} />
-      </div>
-      <h1>{title}</h1>
-      {message && <p>{message}</p>}
+      </IconWrapper>
+      <Title>{title}</Title>
+      {message && <Message>{message}</Message>}
     </StyledEmptyState>
   );
 };
