13d4417b0be9aaee00157be3f01e3567294e6ee0
Victor Lira <victorliradeveloper@gmail.com>
2026-09-18 19:24:11 -0300

    feat: teste 01
    feat: update break point

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   padding: 79px 0;
   margin: 20px auto 0;
-  background: #cf2323;
+  background: #000000;
 `;
 
 export const IconsWrapper = styled.div`
